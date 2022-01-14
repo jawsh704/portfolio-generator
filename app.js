62975b34625ef3e636c31af6d6372b4c09d5599b
YEET <joshuamani@hotmail.com>
2022-01-13 20:12:50 -0500

    html file made with js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
 console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
